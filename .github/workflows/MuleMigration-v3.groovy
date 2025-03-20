e0a62a80a23cdaa8160092e84995d52eeef8861e
mspatagundi34 <mspatagundi34@gmail.com>
2025-03-20 17:53:58 +0530

    Update MuleMigration-v3.groovy

--- .github/workflows/MuleMigration-v3.groovy
+++ .github/workflows/MuleMigration-v3.groovy
@@ -124,7 +124,7 @@ if (!targetNode4) {
 configData.dependenciesToRemove.each {
     conf -> pomxml.dependencies.dependency.each {
         dependency -> if (dependency.groupId == conf.groupId && dependency.artifactId == conf.artifactId) {
-             pomxml.dependencies.remove(dependency)
+             pomxml.dependencies.remove(true)
     	   println("removed '$conf.artifactId' dependency")
         }
     }
